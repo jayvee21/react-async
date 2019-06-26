a807d4ac7ea03511e6cebaef5be653cac34cc443
Jason Glover <jglover@HerculesGroup.local>
2019-06-26 17:44:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Async from 'react-async'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const loadUser = ({ userId }) => 
+  fetch(`https://reqres.in/api/users/${userId}`)
+  .then( res => (res.ok ? res : Promise.reject(res)))
+  .then( res => res.json())
+
+const UserPlaceholder = () => (
+  <div>
+    <div> User Details Loading </div>
+  </div>
+)
+
+const UserDetails = ({ data }) => (
+  <div className="details">
+    <img className="avatar" src={data.data.avatar} alt="" />
+    <div>
+      {data.data.first_name} {data.data.last_name}
     </div>
-  );
+  </div>
+)
+
+const App = () => {
+  return (
+    <Async promiseFn={loadUser} userId={2}>
+      
+      <Async.Pending>
+        <UserPlaceholder />
+      </Async.Pending>
+
+      <Async.Fulfilled>{data => <UserDetails data={data} />}</Async.Fulfilled>
+
+      <Async.Rejected>{error => <p>{error.message}</p>}</Async.Rejected>
+
+    </Async>
+
+
+  )
 }
 
 export default App;
